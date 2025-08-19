c61c51c4b733c8d0768173224bf7695a1821e1e0
Uvarov Oleg <uvarovov.22@edu.ystu.ru>
2025-08-19 11:07:38 +0300

    Revert "1st commit"
    This reverts commit 1a047c3922412e6aae6c4242a2bdb6a5d5529a70.

--- src/main/java/_2/zadanie/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package _2.zadanie.repository;
-
-import _2.zadanie.model.FileData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<FileData, Long> {
-}
