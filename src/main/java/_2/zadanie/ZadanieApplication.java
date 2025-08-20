7972eae4cd2703f502efaa31af2e506765a0bf58
Uvarov Oleg <161945311+GunTo09@users.noreply.github.com>
2025-08-20 15:06:00 +0300

    Revert "added exceptionHandler, method for downloading file and method to emulate file checking"

--- src/main/java/_2/zadanie/ZadanieApplication.java
+++ src/main/java/_2/zadanie/ZadanieApplication.java
@@ -2,10 +2,8 @@ package _2.zadanie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class ZadanieApplication {
 
 	public static void main(String[] args) {
